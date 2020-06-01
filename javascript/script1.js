a9a44591eb47a556b197dd0c47ee9a9c07dc4ad5
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-01 19:22:20 -0300

    [Added] Script learned in video number 8

---
+++ javascript/script1.js
@@ -0,0 +1,26 @@
+// 8. Variables and Data Types
+/**
+ * - The variables names never can start with a number, only can start with _ or $ or letters.
+ * - Types of variables:
+ *      > String
+ *      > Number
+ *      > Boolean
+ *      > Undefined
+ *      > Null
+ */
+
+// strings
+var firstName = "Erick";
+var lastName = "Vicentin";
+
+// numbers
+var age = 23;
+
+// booleans
+var fullAge = true;
+
+// undefined
+var job;
+
+console.log(firstName + " " + lastName + " have: " + age + " years old.");
+console.log(job);
