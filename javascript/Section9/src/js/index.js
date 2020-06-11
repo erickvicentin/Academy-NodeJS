b403fa5a72baaed8d886812a7578d7c48a0fe3e2
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-10 23:48:10 -0300

    [Feat] Added babel setup

--- javascript/Section9/src/js/index.js
+++ javascript/Section9/src/js/index.js
@@ -1,6 +1,6 @@
 // Global app controller
 import testNum from './test';
-
+const x = 12342;
 console.log(
-  `I imported ${testNum} from another module, this module is test.js ;)`
+  `I imported ${testNum} from another module, this module is test.js. Variable x is: ${x} ;)`
 );
--- javascript/Section9/webpack.config.js
+++ javascript/Section9/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/js/index.js',
+  entry: ['@babel/polyfill', './src/js/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js',
@@ -15,4 +15,15 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
