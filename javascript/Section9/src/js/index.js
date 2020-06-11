f3a4f64b34f482c35563226a3333acf0b6dfef88
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-11 14:04:36 -0300

    [Feat] Added pages on results lists

--- javascript/Section9/src/js/index.js
+++ javascript/Section9/src/js/index.js
@@ -1,4 +1,4 @@
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 import Search from './models/Search';
 import * as searchView from './views/searchView';
 
@@ -22,11 +22,13 @@ const controlSearch = async () => {
     // 3) Prepare the UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
@@ -34,4 +36,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', (e) => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
