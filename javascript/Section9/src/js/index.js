4e7ea1f7af3acf268a88ac6776262af46e283090
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-11 02:48:48 -0300

    [Feat] Added store and control search on index.

--- javascript/Section9/src/js/index.js
+++ javascript/Section9/src/js/index.js
@@ -1,6 +1,38 @@
-// Global app controller
-import testNum from './test';
-const x = 12342;
-console.log(
-  `I imported ${testNum} from another module, this module is test.js. Variable x is: ${x} ;)`
-);
+import Search from './models/Search';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes **/
+
+const state = {};
+
+const DOMstrings = {
+  search_btn: '.search',
+};
+
+const controlSearch = async () => {
+  // 1) GET query from view
+  const query = 'pizza'; //TODO
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+
+    // 3) Prepare the UI for results
+
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    console.log(state.search.result);
+  }
+};
+
+document
+  .querySelector(DOMstrings.search_btn)
+  .addEventListener('submit', (e) => {
+    e.preventDefault();
+    controlSearch();
+  });
