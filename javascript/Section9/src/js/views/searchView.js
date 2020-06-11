bd55d2e754b4612849143e098377d47803be5769
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-11 04:45:52 -0300

    [Feat] [UI] Added limit of recipes title method

--- javascript/Section9/src/js/views/searchView.js
+++ javascript/Section9/src/js/views/searchView.js
@@ -10,6 +10,21 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, current) => {
+      if (acc + current.length <= limit) {
+        newTitle.push(current);
+      }
+      return acc + current.length;
+    }, 0);
+    //return the result
+    return `${newTitle.join(' ')}...`;
+  }
+  return title;
+};
+
 const renderRecipe = (recipe) => {
   const markup = `
     <li>
@@ -18,7 +33,7 @@ const renderRecipe = (recipe) => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
