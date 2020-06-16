fd69f2ab9c41546534ea5c6bda1accdd9304f5ab
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-16 19:05:33 -0300

    [FEAT] added test folder and new commands

--- nodejs/notes-app/app.js
+++ nodejs/notes-app/app.js
@@ -1,15 +1,67 @@
 // @node_modules
 const yargs = require('yargs');
 const validator = require('validator');
-const ch = require('chalk');
+const chalk = require('chalk');
 
 // @own_modules
 const notes = require('./notes.js');
 const file = 'notes.txt';
 
-// @app_constants
-console.log(process.argv);
-console.log(yargs.argv);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note content',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (argv) {
+    console.log(chalk.green(`Title: ${argv.title}`));
+    console.log(
+      chalk.yellowBright(`${chalk.underline('Content')}: ${argv.body}`)
+    );
+  },
+});
+
+yargs.command({
+  command: 'edit',
+  describe: 'Edit a note',
+  handler: function () {
+    console.log(chalk.yellowBright('Editing a note'));
+  },
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log(chalk.red('Removing a note'));
+  },
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: function () {
+    console.log(chalk.red('Listing your notes'));
+  },
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log(chalk.red('Read a note'));
+  },
+});
 
 /* if (cmd === 'add') {
   console.log(ch.green('Add: '));
@@ -18,3 +70,5 @@ console.log(yargs.argv);
 } else if (cmd === 'rm') {
   console.log(ch.red('Remove: '));
 } */
+
+yargs.parse();
---
+++ nodejs/playground/1-json.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+/* const note = {
+  title: 'Lista de compras',
+  body: 'Azucar Leche Galletas Harina',
+};
+
+const noteJSON = JSON.stringify(note);
+
+fs.writeFileSync('1-json.json', noteJSON);
+ */
+
+/* const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const user = JSON.parse(dataJSON);
+
+user.name = 'Erick';
+user.age = 23;
+
+const userJSON = JSON.stringify(user);
+
+fs.writeFileSync('1-json.json', userJSON);
+
+console.log(user);
+ */
