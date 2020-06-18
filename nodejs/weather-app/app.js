f8f93e6db0608c0cc57f6ef1a7e6c3ca72c97a22
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-18 16:52:58 -0300

    [Feat] Added catch errorss

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -9,20 +9,42 @@ request(
     json: true,
   },
   (error, response) => {
-    const data = response.body.current;
-    const location = response.body.location;
-    const time = location.localtime.split(' ');
+    const data = response.body;
+    const location = data.location;
 
-    //  Log data
-    console.log(
-      `Ciudad: ${location.name}, ${location.region} - ${location.country}`
-    );
-    console.log(`Fecha: ${time[0]} - Hora: ${time[1]}`);
+    if (error) {
+      console.log('No es posible conectarse con el servicio de clima :(');
+    } else if (data.error) {
+      console.log('No se encuentra esa localizacion.');
+    } else {
+      const time = location.localtime.split(' ');
 
-    console.log(`Temperatura: ${data.temperature}°C`);
-    console.log(`Humedad: ${data.humidity}%`);
-    console.log(`Precipitaciones: ${data.precip}%`);
-    console.log(`Vientos a ${data.wind_speed} km/h`);
-    //  Dont remove
+      //  Log data
+      console.log(
+        `Ciudad: ${location.name}, ${location.region} - ${location.country}`
+      );
+      console.log(`Fecha: ${time[0]} - Hora: ${time[1]}`);
+      console.log(`Temperatura: ${data.current.temperature}°C`);
+      console.log(`Humedad: ${data.current.humidity}%`);
+      console.log(`Precipitaciones: ${data.current.precip}%`);
+      console.log(`Vientos a ${data.current.wind_speed} km/h`);
+      //  Dont remove
+    }
   }
 );
+
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${config.location}.json?access_token=${config.mapToken}&limit=1`;
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const data = response.body;
+
+  if (error) {
+    console.log('No es posible conectarse al servicio.');
+  } else if (data.features.length === 0) {
+    console.log('La localizacion indicada no existe');
+  } else {
+    const latitude = data.features[0].center[0];
+    const longitude = data.features[0].center[1];
+    console.log(latitude, longitude);
+  }
+});
--- nodejs/weather-app/config.js
+++ nodejs/weather-app/config.js
@@ -1,7 +1,10 @@
 const key = '94ae80136220d5d8cbcf9eaa26db6d02';
-const location = 'Resistencia';
+const mapToken =
+  'pk.eyJ1IjoiZXJpY2t2aWNlbnRpbiIsImEiOiJja2JrY3o3aXAwbXVtMzJxbGZ5dGNiOW5oIn0.yHlfYY5oxDYHU03uzBVn8g';
+const location = 'Ressssistencia';
 
 module.exports = {
   key: key,
   location: location,
+  mapToken: mapToken,
 };
