5dcb8694ce9270b8f7865c9aae1f5486ad3c196b
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-18 20:06:05 -0300

    [Feat] Added input place method

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,23 +1,29 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Resistencia', (geoError, geoData) => {
-  if (geoError) {
-    console.log(geoError);
-  } else {
-    console.log(`Localizacion: ${geoData.location}`);
-    const lat = geoData.latitude.toFixed(2);
-    const long = geoData.longitude.toFixed(2);
+const address = process.argv[2];
 
-    forecast(lat, long, (forecastError, forecastData) => {
-      if (forecastError) {
-        console.log(forecastError);
-      } else {
-        console.log(`
+if (!address) {
+  console.log(`Por favor, ingrese un lugar para buscar.`);
+} else {
+  geocode(address, (geoError, geoData) => {
+    if (geoError) {
+      console.log(geoError);
+    } else {
+      console.log(`Localizacion: ${geoData.location}`);
+      const lat = geoData.latitude.toFixed(2);
+      const long = geoData.longitude.toFixed(2);
+
+      forecast(lat, long, (forecastError, forecastData) => {
+        if (forecastError) {
+          console.log(forecastError);
+        } else {
+          console.log(`
         Temperatura: ${forecastData.temperature}°C
         Humedad: ${forecastData.humidity}%
         Vientos a ${forecastData.wind_speed} km/h`);
-      }
-    });
-  }
-});
+        }
+      });
+    }
+  });
+}
