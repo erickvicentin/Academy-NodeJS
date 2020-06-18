6d180b387ca927b122ca5470188ed320945a224a
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-18 19:45:37 -0300

    [Feat] Added forecast Method & callback function

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -1,50 +1,23 @@
-const config = require('./config');
-const request = require('request');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const url = `http://api.weatherstack.com/current?access_key=${config.key}&query=${config.location}`;
-
-request(
-  {
-    url: url,
-    json: true,
-  },
-  (error, response) => {
-    const data = response.body;
-    const location = data.location;
-
-    if (error) {
-      console.log('No es posible conectarse con el servicio de clima :(');
-    } else if (data.error) {
-      console.log('No se encuentra esa localizacion.');
-    } else {
-      const time = location.localtime.split(' ');
-
-      //  Log data
-      console.log(
-        `Ciudad: ${location.name}, ${location.region} - ${location.country}`
-      );
-      console.log(`Fecha: ${time[0]} - Hora: ${time[1]}`);
-      console.log(`Temperatura: ${data.current.temperature}°C`);
-      console.log(`Humedad: ${data.current.humidity}%`);
-      console.log(`Precipitaciones: ${data.current.precip}%`);
-      console.log(`Vientos a ${data.current.wind_speed} km/h`);
-      //  Dont remove
-    }
-  }
-);
-
-const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${config.location}.json?access_token=${config.mapToken}&limit=1`;
-
-request({ url: geocodeURL, json: true }, (error, response) => {
-  const data = response.body;
-
-  if (error) {
-    console.log('No es posible conectarse al servicio.');
-  } else if (data.features.length === 0) {
-    console.log('La localizacion indicada no existe');
+geocode('Resistencia', (geoError, geoData) => {
+  if (geoError) {
+    console.log(geoError);
   } else {
-    const latitude = data.features[0].center[0];
-    const longitude = data.features[0].center[1];
-    console.log(latitude, longitude);
+    console.log(`Localizacion: ${geoData.location}`);
+    const lat = geoData.latitude.toFixed(2);
+    const long = geoData.longitude.toFixed(2);
+
+    forecast(lat, long, (forecastError, forecastData) => {
+      if (forecastError) {
+        console.log(forecastError);
+      } else {
+        console.log(`
+        Temperatura: ${forecastData.temperature}°C
+        Humedad: ${forecastData.humidity}%
+        Vientos a ${forecastData.wind_speed} km/h`);
+      }
+    });
   }
 });
---
+++ nodejs/weather-app/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const config = require('./config');
+
+const forecast = (lat, lon, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=${config.key}&query=${lat},${lon}`;
+
+  request({ url: url, json: true }, (error, response) => {
+    const data = response.body;
+    const location = data.location;
+
+    if (error) {
+      callback('Unable to connect to location services!', undefined);
+    } else if (location.name === null) {
+      callback('Unable to find location. Try another search.', undefined);
+    } else {
+      callback(undefined, {
+        temperature: data.current.temperature,
+        humidity: data.current.humidity,
+        wind_speed: data.current.wind_speed,
+      });
+    }
+  });
+};
+
+module.exports = forecast;
