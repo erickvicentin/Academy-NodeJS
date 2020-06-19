7df202f5c56d1d82a075147200579dcb8470c5a2
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-18 23:54:32 -0300

    [Feat] Code improvements

--- nodejs/weather-app/utils/geocode.js
+++ nodejs/weather-app/utils/geocode.js
@@ -6,15 +6,17 @@ const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${config.mapToken}&limit=1`;
 
   request({ url: url, json: true }, (error, response) => {
+    const geoData = response.body.features[0];
+
     if (error) {
       callback('Unable to connect to location services!', undefined);
     } else if (response.body.features.length === 0) {
       callback('Unable to find location. Try another search.', undefined);
     } else {
       callback(undefined, {
-        location: response.body.features[0].place_name,
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
+        location: geoData.place_name,
+        latitude: geoData.center[1],
+        longitude: geoData.center[0],
       });
     }
   });
