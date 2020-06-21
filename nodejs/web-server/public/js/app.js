e2a04c041b1201f02961dd721c1ec601525ff9d5
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-21 01:30:43 -0300

    [Feat] Added fetch data to client side

--- nodejs/web-server/public/js/app.js
+++ nodejs/web-server/public/js/app.js
@@ -1,1 +1,14 @@
 console.log('Client side javascript file is loaded!');
+
+fetch('http://localhost:3000/weather?address=Resistencia').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(`Temperatura: ${data.forecast.temperature}°C`);
+      console.log(`Humedad: ${data.forecast.humidity}%`);
+      console.log(`Vientos a: ${data.forecast.wind_speed} km/h`);
+    }
+  });
+});
