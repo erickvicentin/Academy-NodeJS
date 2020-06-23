99e62cb0007a6d1c7e9f137a277910ac6d3d10cb
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-23 20:17:50 -0300

    [Feat] Added method for read documents

--- nodejs/task-manager/mongodb.js
+++ nodejs/task-manager/mongodb.js
@@ -3,10 +3,6 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const objectID = mongodb.ObjectID;
 
-const id = new objectID();
-console.log(id);
-console.log(id.getTimestamp());
-
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
@@ -22,57 +18,6 @@ MongoClient.connect(
     }
 
     // Create Read Update & Delete
-
     const db = client.db(databaseName);
-
-    db.collection('users').insertOne(
-      {
-        _id: id,
-        name: 'Hercules',
-        age: 29,
-      },
-      (error, result) => {
-        if (error) {
-          return console.log('Unable to insert document.');
-        }
-
-        console.log(result.ops);
-      }
-    );
-
-    /* db.collection('users').insertMany(
-      [
-        {
-          name: 'Juani',
-          age: 2,
-        },
-        {
-          name: 'Lucy',
-          age: 10,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log('Unable to insert documents!');
-        }
-
-        console.log(result.ops);
-      }
-    ); */
-
-    /* db.collection('tasks').insertMany(
-      [
-        { description: 'Finish the nodejs course', completed: false },
-        { description: 'Buy a new shoes', completed: false },
-        { description: 'Read a book', completed: true },
-      ],
-      (error, result) => {
-        if (error) {
-          return console.log('Unable to insert tasks');
-        }
-
-        console.log(result.ops);
-      }
-    ); */
   }
 );
