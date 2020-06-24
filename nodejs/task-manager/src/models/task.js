4faf43c4bf1daac704a15a80bcb761f5b1003acc
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-24 19:57:00 -0300

    [Feat] Added task model

---
+++ nodejs/task-manager/src/models/task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const Task = mongoose.model('Task', {
+  description: {
+    required: true,
+    trim: true,
+    type: String,
+  },
+  completed: {
+    default: false,
+    type: Boolean,
+  },
+});
+
+module.exports = Task;
