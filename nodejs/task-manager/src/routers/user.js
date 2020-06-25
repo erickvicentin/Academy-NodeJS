10378faf00480efacfab3bcc7608db5b70e82074
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-25 06:21:38 -0300

    [Feat] refactoring how user & task are updated

--- nodejs/task-manager/src/routers/user.js
+++ nodejs/task-manager/src/routers/user.js
@@ -48,10 +48,11 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(_id);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+
+    //const user = await User.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true});
 
     if (!user) {
       return res.status(404).send();
