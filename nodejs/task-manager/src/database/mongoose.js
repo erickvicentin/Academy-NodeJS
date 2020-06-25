51e13fdebf2665c1d3924aa71a9b7ab9ce76bbbd
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-25 06:20:04 -0300

    [Fix] bugs with properties

--- nodejs/task-manager/src/database/mongoose.js
+++ nodejs/task-manager/src/database/mongoose.js
@@ -5,5 +5,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: true,
+  useFindAndModify: false,
 });
