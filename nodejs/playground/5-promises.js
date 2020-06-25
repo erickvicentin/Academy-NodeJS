5770a07a84f8d9cc4fe64a0c2e597787ab42067a
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-25 00:42:29 -0300

    [Feat] Added promises chaning example

--- nodejs/playground/5-promises.js
+++ nodejs/playground/5-promises.js
@@ -1,4 +1,4 @@
-const doWorkPromise = new Promise((resolve, reject) => {
+/* const doWorkPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
     //resolve([1, 3, 5]);
     reject('Things went wrong');
@@ -12,7 +12,7 @@ doWorkPromise
   .catch((error) => {
     console.log('Error: ', error);
   });
-
+ */
 //                              --> FULFILLED
 //                             |
 //                            |
@@ -20,3 +20,23 @@ doWorkPromise
 //                            \
 //                             \
 //                              --> REJECTED
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+add(1, 1)
+  .then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+  })
+  .then((sum2) => {
+    console.log(sum2);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
