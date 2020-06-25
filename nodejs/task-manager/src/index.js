f979334029225f81d98d911703c3b0a72fc2e063
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-25 04:30:57 -0300

    [Fix]  bugs with express

--- nodejs/task-manager/src/index.js
+++ nodejs/task-manager/src/index.js
@@ -1,7 +1,5 @@
 // @node_modules
 const express = require('express');
-const Task = require('./models/task');
-const User = require('./models/user');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 require('./database/mongoose');
@@ -13,8 +11,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-const router = new express.Router();
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
