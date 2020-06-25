6cc20d9c1465bec6ee2d77f79bc88e3e760a25ae
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-25 06:22:22 -0300

    [Feat] Added function to encrypt the password

--- nodejs/task-manager/src/models/user.js
+++ nodejs/task-manager/src/models/user.js
@@ -1,7 +1,9 @@
+// @node_modules
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     required: true,
     trim: true,
@@ -45,5 +47,17 @@ const User = mongoose.model('User', {
     },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
