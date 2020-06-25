a78afc49f858eb7afc0f17c38c9f7e4bfe574f44
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-25 16:57:49 -0300

    [Feat] Added user autentification

--- nodejs/task-manager/src/models/user.js
+++ nodejs/task-manager/src/models/user.js
@@ -12,6 +12,7 @@ const userSchema = new mongoose.Schema({
   email: {
     lowercase: true,
     required: true,
+    unique: true,
     type: String,
     trim: true,
     validate(value) {
@@ -48,6 +49,23 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this;
 
