8e105139d878a215c5622d4fa88f49e382a38b5b
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-26 08:01:50 -0300

    [Feat] Added mongooseSchema & timeStamps

--- nodejs/task-manager/src/models/task.js
+++ nodejs/task-manager/src/models/task.js
@@ -1,21 +1,28 @@
 // @node_modules
 const mongoose = require('mongoose');
 
-const Task = mongoose.model('Task', {
-  description: {
-    required: true,
-    trim: true,
-    type: String,
+const taskSchemma = new mongoose.Schema(
+  {
+    description: {
+      required: true,
+      trim: true,
+      type: String,
+    },
+    completed: {
+      default: false,
+      type: Boolean,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
   },
-  completed: {
-    default: false,
-    type: Boolean,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model('Task', taskSchemma);
 
 module.exports = Task;
--- nodejs/task-manager/src/models/user.js
+++ nodejs/task-manager/src/models/user.js
@@ -7,59 +7,64 @@ const validator = require('validator');
 // @app_modules
 const Task = require('./task');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    required: true,
-    trim: true,
-    type: String,
-  },
-  email: {
-    lowercase: true,
-    required: true,
-    unique: true,
-    type: String,
-    trim: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email format not valid');
-      }
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      required: true,
+      trim: true,
+      type: String,
     },
-  },
-  age: {
-    default: 0,
-    type: Number,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number');
-      }
-    },
-  },
-  country: {
-    default: 'argentina',
-    lowercase: true,
-    trim: true,
-    type: String,
-  },
-  password: {
-    minlength: 6,
-    required: true,
-    trim: true,
-    type: String,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error(`The password cant be 'password'.`);
-      }
-    },
-  },
-  tokens: [
-    {
-      token: {
-        type: String,
-        required: true,
+    email: {
+      lowercase: true,
+      required: true,
+      unique: true,
+      type: String,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email format not valid');
+        }
       },
     },
-  ],
-});
+    age: {
+      default: 0,
+      type: Number,
+      validate(value) {
+        if (value < 0) {
+          throw new Error('Age must be a positive number');
+        }
+      },
+    },
+    country: {
+      default: 'argentina',
+      lowercase: true,
+      trim: true,
+      type: String,
+    },
+    password: {
+      minlength: 6,
+      required: true,
+      trim: true,
+      type: String,
+      validate(value) {
+        if (value.toLowerCase().includes('password')) {
+          throw new Error(`The password cant be 'password'.`);
+        }
+      },
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Delete user tasks then user is removed
 userSchema.pre('remove', async function (next) {
