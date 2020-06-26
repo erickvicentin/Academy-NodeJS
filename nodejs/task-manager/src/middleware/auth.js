5b83317bbdfe18a0755775b939daaae81c6580b7
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-26 03:19:25 -0300

    [Feat] Added user authenticator

---
+++ nodejs/task-manager/src/middleware/auth.js
@@ -0,0 +1,27 @@
+// @ node_modules
+const jwt = require('jsonwebtoken');
+
+// @ app_modules
+const User = require('../models/user');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, 'task-app-vicerick');
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
+
+    if (!user) {
+      throw new Error('Unable to login.');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'Please authenticate.' });
+  }
+};
+
+module.exports = auth;
