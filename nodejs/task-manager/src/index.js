275844bff16b49788cda36d3afa7fd14351e1446
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-27 13:35:08 -0300

    [Feat] Code improvements

--- nodejs/task-manager/src/index.js
+++ nodejs/task-manager/src/index.js
@@ -8,7 +8,7 @@ const taskRouter = require('./routers/task');
 
 // @app_constants
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
