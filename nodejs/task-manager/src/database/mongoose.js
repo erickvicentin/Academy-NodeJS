655e22d2deec6de7511bb6f58d4b495ef5c8ccfa
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-27 15:03:58 -0300

    [Feat] Changed keys

--- nodejs/task-manager/src/database/mongoose.js
+++ nodejs/task-manager/src/database/mongoose.js
@@ -1,7 +1,8 @@
 // @node_modules
 const mongoose = require('mongoose');
+const url = process.env.MONGODB_URL;
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect(url, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
