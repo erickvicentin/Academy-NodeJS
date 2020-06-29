2c751e76d3a2ad52b51bcdee318422a0d09de656
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-29 13:25:31 -0300

    [Feat] Added listeners and geolocation

--- nodejs/chat-app/public/js/chat.js
+++ nodejs/chat-app/public/js/chat.js
@@ -1,1 +1,35 @@
-io();
+const socket = io();
+
+socket.on('message', (message) => {
+  console.log(message);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message);
+});
+
+/* socket.on('countUpdated', (count) => {
+  console.log('The count has been updated', count);
+});
+
+document.querySelector('#increment').addEventListener('click', () => {
+  socket.emit('increment');
+});
+ */
+
+document.querySelector('#geo-share').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
