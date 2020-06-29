8aec75c99d6f31c113092917c8b54317d043a9af
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-29 16:02:11 -0300

    [Feat] Added location-message template

--- nodejs/chat-app/public/js/chat.js
+++ nodejs/chat-app/public/js/chat.js
@@ -1,15 +1,47 @@
 const socket = io();
 
+// @App_elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = document.querySelector('input');
+const $messageFormButton = document.querySelector('#send');
+const $geoShareButton = document.querySelector('#geo-share');
+const $messages = document.querySelector('#messages');
+
+// @Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector(
+  '#location-message-template'
+).innerHTML;
+
 socket.on('message', (message) => {
-  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message,
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+socket.on('locationMessage', (url) => {
+  console.log(url);
+  const html = Mustache.render(locationMessageTemplate, {
+    url,
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
+});
+
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  //disable send button while the message is sending
+  $messageFormButton.setAttribute('disabled', 'disabled');
 
   const message = e.target.elements.message.value;
 
   socket.emit('sendMessage', message, (error) => {
+    // enable button when the message was delivered
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -18,20 +50,13 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
   });
 });
 
-/* socket.on('countUpdated', (count) => {
-  console.log('The count has been updated', count);
-});
-
-document.querySelector('#increment').addEventListener('click', () => {
-  socket.emit('increment');
-});
- */
-
-document.querySelector('#geo-share').addEventListener('click', () => {
+// Get location and send when click on button
+$geoShareButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser');
   }
 
+  $geoShareButton.setAttribute('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
       'sendLocation',
@@ -40,6 +65,7 @@ document.querySelector('#geo-share').addEventListener('click', () => {
         longitude: position.coords.longitude,
       },
       (message) => {
+        $geoShareButton.removeAttribute('disabled');
         console.log(message);
       }
     );
