5e4e1328ec34cdf877abd9ef705922db0fb00d2b
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-29 00:27:40 -0300

    [Feat] Added add method

--- nodejs/task-manager/math.js
+++ nodejs/task-manager/math.js
@@ -6,16 +6,18 @@ const celsiusToFahrenheit = (temp) => {
   return temp * 1.8 + 32;
 };
 
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) reject('Numbers must be non-negatives');
+
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
 module.exports = {
   celsiusToFahrenheit,
   fahrenheitToCelsius,
+  add,
 };
-
-//
-
-// Goal: Test temperature conversion functions
-//
-// 1. Export both functions and load them into test suite
-// 2. Create "Should convert 32 F to 0 C"
-// 3. Create "Should convert 0 C to 32 F"
-// 4. Run the Jest to test your work!
