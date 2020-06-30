6d6f68066ac9283e26b0b4e3a1a1e93afff8995e
Erick Vicentin <erickvicentin16@gmail.com>
2020-06-30 01:50:34 -0300

    [Feat] Added usersInRoom list & autoScroll

--- nodejs/chat-app/public/js/chat.js
+++ nodejs/chat-app/public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages');
 
 // @Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 const locationMessageTemplate = document.querySelector(
   '#location-message-template'
 ).innerHTML;
@@ -19,22 +20,56 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
+const autoscroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild;
+
+  // Height of the new message
+  const newMessagleStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessagleStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  //Visible Height
+  const visibleHeight = $messages.offsetHeight;
+
+  // Height of messages container
+  const containerHeight = $messages.scrollHeight;
+
+  // How far have I scrolled?
+  const scroolOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scroolOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
 socket.on('message', (message) => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format('h:mm a'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
+  autoscroll();
 });
 
 socket.on('locationMessage', (message) => {
   console.log(message);
   const html = Mustache.render(locationMessageTemplate, {
+    username: message.username,
     url: message.url,
     createdAt: moment(message.createdAt).format('h:mm a'),
   });
   $messages.insertAdjacentHTML('beforeend', html);
+});
+
+socket.on('roomData', ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector('#sidebar').innerHTML = html;
 });
 
 $messageForm.addEventListener('submit', (e) => {
@@ -55,6 +90,7 @@ $messageForm.addEventListener('submit', (e) => {
 
     console.log('Message delivered!');
   });
+  autoscroll();
 });
 
 $sendLocationButton.addEventListener('click', () => {
--- nodejs/chat-app/src/index.js
+++ nodejs/chat-app/src/index.js
@@ -39,31 +39,41 @@ io.on('connection', (socket) => {
 
     socket.join(user.room);
 
-    socket.emit('message', generateMessage('Welcome!'));
+    socket.emit('message', generateMessage('Admin', 'Welcome to chatApp!'));
     socket.broadcast
       .to(user.room)
-      .emit('message', generateMessage(`${user.username} has joined!`));
+      .emit(
+        'message',
+        generateMessage('Admin', `${user.username} has joined!`)
+      );
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
     callback();
   });
 
   // SEND MESSAGE TO ROOM
   socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!');
     }
-
-    io.emit('message', generateMessage(message));
+    io.to(user.room).emit('message', generateMessage(user.username, message));
     callback();
   });
 
   // SEND LOCATION TO ROOM
   socket.on('sendLocation', (coords, callback) => {
-    io.emit(
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit(
       'locationMessage',
       generateLocationMessage(
+        user.username,
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
       )
     );
@@ -78,8 +88,12 @@ io.on('connection', (socket) => {
     if (user) {
       io.to(user.room).emit(
         'message',
-        generateMessage(`${user.username} has left!`)
+        generateMessage('Admin', `${user.username} has left!`)
       );
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
     }
   });
 });
